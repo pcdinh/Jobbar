63101809b6f16b5af4027e01f1d878aee93fd3b1
Umut AYDIN <umutumu@gmail.com>
2010-12-06 15:01:38 +0200

    single mode!

--- src/lib/server.py
+++ src/lib/server.py
@@ -1,19 +1,22 @@
 from twisted.internet import protocol
 
+import random
 import simplejson as json
+import uuid
 
 class SocketHandler(protocol.Protocol):
     def connectionMade(self):
-        print self.transport.doWrite()
-        print "connection from %s" % self.transport.getPeer().host
+        #print "connection from %s" % self.transport.getPeer().host
+        pass
 
     def connectionLost(self, reason):
-        print "disconnected (%s)" % reason.getErrorMessage()
+        #print "disconnected (%s)" % reason.getErrorMessage()
+        self.unregisterBySocket()
 
     def dataReceived(self, data):
         response = self.requestHandler(data.strip())
         if response != None:
-            self.transport.write(response);
+            self.transport.write(response + "\n\r");
 
     def requestHandler(self, request):
         try:
@@ -21,16 +24,24 @@ class SocketHandler(protocol.Protocol):
         except:
             return '{"status": 0, "error": "invalid input format"}'
 
+        if data.has_key('params'):
+            params = data.get('params')
+        else:
+            return '{"status": 0, "error": "missing parameter: params (parameters)"}'
+
         if data.has_key('cmd'):
             command = data.get('cmd')
             if command == 'register':
-                return None
+                return self.register(params.get('name'))
             elif command == 'unregister':
-                return None
+                return self.unregisterByName(params.get('name'))
             elif command == 'call':
-                return None
+                if params.get('bg') == False:
+                    params.put('uuid', uuid.uuid1())
+
+                return self.call(params.get('name'), params, params.get('bg'))
             elif command == 'response':
-                return None
+                return self.response(data)
             else:
                 return '{"status": 0, "error": "unknown command"}'
         else:
@@ -42,11 +53,11 @@ class SocketHandler(protocol.Protocol):
     This method adds a new job to local list and shares it with remote servers
     """
     def register(self, name):
-        if not self.factory.jobs.get("local").has_key(name):
-            self.factory.jobs.get("local").put(name, [])
+        if not self.factory.jobs.get('local').has_key(name):
+            self.factory.jobs.get('local').put(name, [])
 
-        if self.transport not in self.factory.jobs.get("local").get(name):
-            self.factory.jobs.get("local").get(name).append(self.transport)
+        if self.transport not in self.factory.jobs.get('local').get(name):
+            self.factory.jobs.get('local').get(name).append(self.transport)
 
         # call synchronization method
 
@@ -55,9 +66,9 @@ class SocketHandler(protocol.Protocol):
     This method removes a job from local list by job name and shares it with remote servers
     """
     def unregisterByName(self, name):
-        if self.factory.jobs.get("local").has_key(name):
+        if self.factory.jobs.get('local').has_key(name):
             #if self.transport in self.factory.jobs.get("local").get(name):
-            self.factory.jobs.get("local").get(name).remove(self.transport)
+            self.factory.jobs.get('local').get(name).remove(self.transport)
 
         # call synchronization method
 
@@ -65,8 +76,8 @@ class SocketHandler(protocol.Protocol):
     This method removes jobs from local list by socket when connection closed with worker and shares it with remote servers
     """
     def unregisterBySocket(self):
-        for job in self.factory.jobs.get("local"):
-            self.factory.jobs.get("local").get(job).remove(self.transport)
+        for job in self.factory.jobs.get('local'):
+            self.factory.jobs.get('local').get(job).remove(self.transport)
 
         # call synchronization method
 
@@ -78,9 +89,9 @@ class SocketHandler(protocol.Protocol):
     """
     def call(self, name, params, background):
         worker = self.getWorkerSocket(name)
-        if worker != False:
+        if worker != None:
             if not background:
-                self.factory.requests.get("local").put(params.get("uid"), self.transport)
+                self.factory.requests.get('local').put(params.get('uuid'), self.transport)
 
             worker.write(json.dumps(params));
             return None
@@ -93,12 +104,29 @@ class SocketHandler(protocol.Protocol):
     Background job requests have no response.
     """
     def response(self, data):
-        pass
+        if data.get('params').has_key('uuid'):
+            process = data.get('params').get('uuid')
+            if self.factory.requests.get('local').has_key(process):
+                del data.get('params')['uuid']
+                self.factory.requests.get('local').get(process)[process].write(data.get('params'))
+                del self.factory.requests.get('local').get(process)[process]
+
+            # else: unknown uuid!
+
+        return None
 
     """
     @param  String  name: Worker name
     This method returns a transport instance suitable with the given job
     """
     def getWorkerSocket(self, name):
-        return False
+        if self.factory.jobs.has_key(name) and (len(self.factory.jobs[name]) > 0):
+            worker = random.sample(self.factory.jobs.get(name), 1)
+            if len(worker) > 0:
+                # Still connected? If not we need to call this method again!
+                return worker
+            else:
+                return None
+
+        return None
     # INSTRUCTION SET - End
